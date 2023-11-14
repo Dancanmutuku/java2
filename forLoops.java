b08c64f63eff667e6c8267f0a3aac5c84d021802
Dancanmutuku <133522453+Dancanmutuku@users.noreply.github.com>
2023-11-14 12:16:57 +0300

    change

--- forLoops.java
+++ forLoops.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class forLoops {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++)
+        for(int i=0;i<2;i++)
         System.out.println("Just advancing");
+        Scanner scanner=new Scanner(System.in);
+        String input="";
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input=scanner.next().toLowerCase();
+            if (input.equals("pass")) {
+                continue;
+            }
+            if (!input.equals("quit")) {
+                System.out.println(input); 
+            }
+            System.out.println(input);
+            scanner.close();
+        }
     }
 }
